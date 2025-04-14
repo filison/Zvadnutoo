14a5e68ac6c3951d945b46c16af65d3aae948eb3
filison <65769401+filison@users.noreply.github.com>
2025-04-14 14:59:54 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAcg0qqzWJwkxmopkTgzfasMqd3siQNxIU",
   authDomain: "zvladnuto-4e6cb.firebaseapp.com",
   projectId: "zvladnuto-4e6cb",
-  storageBucket: "zvladnuto-4e6cb.appspot.com", // OPRAVA ZDE
+  storageBucket: "zvladnuto-4e6cb.firebasestorage.app", // špatně, // OPRAVA ZDE
   messagingSenderId: "299881468872",
   appId: "1:299881468872:web:8c36e215cf40a91748e6fd",
   measurementId: "G-HZZG7JRYCC"
